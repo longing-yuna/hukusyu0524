c4f8be0a648127473801dabf57ab985c4cfbdc6f
pizza_girl <pizza.girl.dat@gmail.com>
2019-05-24 13:31:13 +0900

    ao-i

--- hukusyu0524/Form1.cs
+++ hukusyu0524/Form1.cs
@@ -12,9 +12,54 @@ namespace hukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = 5;
+            vy=0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = -5;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 5;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx = -5;
+            vy = 0;
         }
     }
 }
